80d5ae306360a1aba26fb706f1efce4e2456bc19
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-07 06:53:09 +0000

    [Add]callback

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+require "nkf"
+
 class Order < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 40 }
@@ -5,5 +7,20 @@ class Order < ApplicationRecord
   validates :email, presence: true, length: { maximum: 100 }, email_format: true
   validates :telephone, presence: true, length: { maximum: 11 }, numericality: { only_integer: true }
   validates :delivery_address, presence: true, length: { maximum: 100 }
-  
+
+  after_initialize :format_telephone
+  after_initialize :format_email
+
+  private
+
+  def format_telephone
+    return if telephone.blank?
+    self.telephone = telephone.tr( "０-９", "0-9").delete("^0-9")
+  end
+
+  def format_email
+    return if email.blank?
+    self.email = NKF.nkf("-w -Z4", email)
+  end
+
 end
