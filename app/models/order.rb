a4e34bde9c2b752c6e2ba8ad4bf4bddacf20a633
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-07 06:23:42 +0000

    [Add]each_validator

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 40 }
+  # バリデーターと紐付ける
+  validates :email, presence: true, length: { maximum: 100 }, email_format: true
+  validates :telephone, presence: true, length: { maximum: 11 }, numericality: { only_integer: true }
+  validates :delivery_address, presence: true, length: { maximum: 100 }
+  
 end
---
+++ app/validators/email_format_validator.rb
@@ -0,0 +1,10 @@
+class EmailFormatValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+
+    return if /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match?(value)
+
+    record.errors.add(attribute, "形式が正しくありません")
+  end
+
+end
